01eb797903acd612812528b2c179d20f45177c63
Vatsal Singh <vatsalsinghkv@gmail.com>
2021-06-25 13:59:51 +0530

    script formatting changed

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-"use strict";
+'use strict';
 
 // Selecting Elements
-const diceEl = document.querySelector(".dice");
 
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
+const diceEl = document.querySelector('.dice');
 
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Variables
 let activePlayer = 0;
 let hasNotWon = true;
 
@@ -19,55 +21,55 @@ let name = [];
 
 // FUNCTIONS
 
-// Modal
-
+// Modal-Form
 const formValidation = () => {
-  name.push(document.getElementById("name-0").value);
-  name.push(document.getElementById("name-1").value);
+  name.push(document.getElementById('name-0').value);
+  name.push(document.getElementById('name-1').value);
 
   if (name[0]) {
     name[0] = name[0].length > 12 ? name[0].slice(0, 12) : name[0];
-    document.getElementById("name--0").textContent = name[0];
+    document.getElementById('name--0').textContent = name[0];
   }
   if (name[1]) {
     name[1] = name[1].length > 12 ? name[1].slice(0, 12) : name[1];
-    document.getElementById("name--1").textContent = name[1];
+    document.getElementById('name--1').textContent = name[1];
   }
 };
 
+// Modal
 const openModal = () => {
-  let modal = document.querySelector(".modal");
-  let closeBtn = document.querySelector(".close-modal");
-  let overlay = document.querySelector(".overlay");
-  let submitBtn = document.querySelector(".submit-btn");
+  let modal = document.querySelector('.modal');
+  let closeBtn = document.querySelector('.close-modal');
+  let overlay = document.querySelector('.overlay');
+  let submitBtn = document.querySelector('.submit-btn');
 
   showModal();
 
-  closeBtn.addEventListener("click", closeModal);
-  overlay.addEventListener("click", closeModal);
-  submitBtn.addEventListener("click", () => {
+  closeBtn.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+  submitBtn.addEventListener('click', () => {
     closeModal();
     formValidation();
   });
 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape") closeModal();
-    else if (e.key == "Enter") {
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') closeModal();
+    else if (e.key == 'Enter') {
       closeModal();
       formValidation();
     }
   });
 
   function showModal() {
-    overlay.classList.remove("hidden");
-    modal.classList.remove("hidden");
-    closeBtn.classList.remove("hidden");
+    overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+    closeBtn.classList.remove('hidden');
   }
 
   function closeModal() {
-    overlay.classList.add("hidden");
-    modal.classList.add("hidden");
-    closeBtn.classList.add("hidden");
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    closeBtn.classList.add('hidden');
   }
 };
 
@@ -84,28 +86,26 @@ const switchPlayer = () => {
   refresh();
   document
     .querySelector(`.player--${activePlayer}`)
-    .classList.remove("player--active");
+    .classList.remove('player--active');
 
   activePlayer = activePlayer == 1 ? 0 : 1;
 
   document
     .querySelector(`.player--${activePlayer}`)
-    .classList.add("player--active");
+    .classList.add('player--active');
 };
 
 const reset = () => {
   if (!hasNotWon) {
     document
-      .querySelector(".player--winner")
-      .classList.remove("player--winner");
+      .querySelector('.player--winner')
+      .classList.remove('player--winner');
 
     if (name[activePlayer]) {
       document.querySelector(`#name--${activePlayer}`).textContent =
         name[activePlayer];
     } else {
-      document.querySelector(`#name--${activePlayer}`).textContent = `Player-${
-        activePlayer + 1
-      }`;
+      document.querySelector(`#name--${activePlayer}`).textContent = `Player ${activePlayer + 1}`;
     }
 
     hasNotWon = true;
@@ -122,25 +122,23 @@ const reset = () => {
   switchPlayer();
 
   activePlayer = 0;
-  document.querySelector(".player--active").classList.remove("player--active");
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.add("player--active");
+  document.querySelector('.player--active').classList.remove('player--active');
+  document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
 
-  diceEl.classList.add("hidden");
+  diceEl.classList.add('hidden');
 };
 
 reset();
 openModal();
 
 // Dice Roll functionality
-btnRoll.addEventListener("click", () => {
+btnRoll.addEventListener('click', () => {
   if (hasNotWon) {
     refresh();
 
     const dice = Math.floor(Math.random() * 6) + 1;
 
-    diceEl.classList.remove("hidden");
+    diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
     if (dice == 1) {
@@ -154,7 +152,7 @@ btnRoll.addEventListener("click", () => {
 });
 
 // Hold Btn Functionallity
-btnHold.addEventListener("click", () => {
+btnHold.addEventListener('click', () => {
   if (hasNotWon) {
     refresh();
 
@@ -166,14 +164,9 @@ btnHold.addEventListener("click", () => {
       if (finalScore >= 50) {
         hasNotWon = false;
 
-        document
-          .querySelector(`.player--${activePlayer}`)
-          .classList.add("player--winner");
-
-        document.querySelector(`#name--${activePlayer}`).textContent =
-          "Winner! 🎉";
-
-        diceEl.classList.add("hidden");
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`#name--${activePlayer}`).textContent = 'Winner! 🎉';
+        diceEl.classList.add('hidden');
       } else {
         switchPlayer();
       }
@@ -181,4 +174,4 @@ btnHold.addEventListener("click", () => {
   }
 });
 
-btnNew.addEventListener("click", reset);
+btnNew.addEventListener('click', reset);
